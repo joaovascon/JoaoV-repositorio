a132503af623bdc8f48d9661c06e808b772b70ee
joaovascon <joao.vascon.11@gmail.com>
2025-08-01 12:32:25 +0100

    ultima aula

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+const hamburger = document.getElementById('hamburger');
+const sideMenu = document.getElementById('sideMenu');
+
+hamburger.addEventListener('click', () => {
+    sideMenu.classList.toggle('show');
+});
